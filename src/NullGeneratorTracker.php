7048642571c93f37b8632bb3739de5d68c711889
Carlos C Soto <eclipxe13@gmail.com>
2023-12-19 01:29:52 -0600

    Add comments to null generator tracker

--- src/NullGeneratorTracker.php
+++ src/NullGeneratorTracker.php
@@ -8,21 +8,26 @@ class NullGeneratorTracker implements GeneratorTrackerInterface
 {
     public function boot(): void
     {
+        // Null Object
     }
 
     public function type(Data\Type $type): void
     {
+        // Null Object
     }
 
     public function segment(Data\Segment $segment): void
     {
+        // Null Object
     }
 
     public function family(Data\Family $family): void
     {
+        // Null Object
     }
 
     public function class(Data\Classification $class): void
     {
+        // Null Object
     }
 }
